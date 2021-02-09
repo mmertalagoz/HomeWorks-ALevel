76457b23c9fd84cf952c2053d85c163843430868
Mucahit Mert Alagoz <mmertalagoz10@gmail.com>
2021-02-09 06:20:44 +0200

    TicTacToe Added

---
+++ TicTacToe/script.js
@@ -0,0 +1,105 @@
+const blocks = document.querySelectorAll('.block');
+const playerText = document.getElementById('player');
+const errorText = document.getElementById('error');
+const restart = document.getElementById('restart');
+let player = 'X';
+let gameOver = false;
+let winner;
+
+function startGame(){
+    playerText.textContent = `${player}'s Turn !`
+    blocks.forEach(block=>block.addEventListener('click',() => chooseArea(block)))
+}
+
+function chooseArea(block){
+    if(block.textContent===''){
+        block.textContent = player;
+        turnPlayer();
+    }else{
+        errorText.textContent = 'This box is a Busy !';
+        block.style.border = '2px solid red'
+        setTimeout(()=>{
+        errorText.textContent='';
+        block.style.border='1px solid black'
+        },2000)
+    }
+    
+    controlWin();
+    
+    controlTie();
+
+    if(gameOver){
+        playerText.textContent = `Game is Over ${winner} WON!`
+        blocks.forEach(block=>block.style.pointerEvents = 'none')
+    }
+}
+
+function turnPlayer(){
+    if (player==='X') {
+        player = 'O';
+        playerText.textContent=`${player}'s Turn !`
+        return;
+    } else if (player==='O'){
+        player = 'X';   
+        playerText.textContent=`${player}'s Turn !`
+    }
+}
+function controlWin(){
+    controlRows();
+    controlColumns();
+}
+function controlTie(){
+    //if 0-0
+    const values = [];
+    blocks.forEach(block=>values.push(block.textContent))
+    // здесь мы создаем массив и по индексам дивов пушим в массив и если массив уже забит то ничья..
+    if(!values.includes('')){
+        playerText.textContent = 'No Win' 
+        blocks.forEach(block=>block.style.pointerEvents='none')
+    }
+}
+
+function controlRows(){
+
+    let row1=blocks[0].textContent == blocks[1].textContent && 
+    blocks[0].textContent == blocks[2].textContent && blocks[0].textContent !== '';
+
+    let row2=blocks[3].textContent == blocks[4].textContent && 
+    blocks[3].textContent == blocks[5].textContent && blocks[3].textContent !== '';
+
+    let row3=blocks[6].textContent == blocks[7].textContent && 
+    blocks[6].textContent == blocks[8].textContent && blocks[6].textContent !== '';
+
+    if( row1 || row2 || row3 ) {
+    gameOver = true;
+        }
+    if(row1) return winner = blocks[0].textContent
+    if(row2) return winner = blocks[3].textContent
+    if(row3) return winner = blocks[6].textContent
+    }
+
+function controlColumns(){
+
+    let col1=blocks[0].textContent == blocks[3].textContent && 
+    blocks[0].textContent == blocks[6].textContent && blocks[0].textContent !== '';
+
+    let col2=blocks[1].textContent == blocks[4].textContent && 
+    blocks[1].textContent == blocks[7].textContent && blocks[1].textContent !== '';
+
+    let col3=blocks[2].textContent == blocks[5].textContent && 
+    blocks[2].textContent == blocks[8].textContent && blocks[2].textContent !== '';
+
+    if( col1 || col2 || col3 ) {
+    gameOver = true;
+        }
+    if(col1) return winner = blocks[0].textContent
+    if(col2) return winner = blocks[1].textContent
+    if(col3) return winner = blocks[2].textContent
+    }
+
+    restart.addEventListener('click',reloadPage)
+    function reloadPage(){
+        window.location.reload()
+    }
+
+startGame();
