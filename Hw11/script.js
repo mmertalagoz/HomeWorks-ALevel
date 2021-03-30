511782da6b5c45b4b177eaad977d90b2d917a2bf
Mucahit Mert Alagoz <mmertalagoz10@gmail.com>
2021-03-30 04:52:22 +0300

    done

---
+++ Hw11/script.js
@@ -0,0 +1,30 @@
+// hw1
+// const myName = prompt('Enter Your Name');
+// const myAge = prompt('Enter Your Age');
+// document.cookie = `${myName} , ${myAge}`;
+
+// console.log(document.cookie);
+
+// hw2
+
+// не вышло
+
+// hw3
+
+// const textarea = document.getElementById('textarea');
+// const button = document.getElementById('button');
+
+// button.addEventListener('click', function () {
+// 	localStorage.setItem('text', textarea.value);
+// });
+
+// if (localStorage['text']) {
+// 	const saveText = localStorage.getItem('text');
+// 	textarea.innerHTML = saveText;
+// }
+
+// hw4
+
+// const danmassiv = [1, 2, 3, 4, 5];
+// danmassiv.splice(1, 'a', 'b', 2, 3, 4, 'c', 5, 'e');
+// console.log(danmassiv);
